a030b2616d7a63c41cca871f69cb1c69d00d3b85
Pedrovin1 <pedro.zancan18@gmail.com>
2025-01-31 15:41:19 -0300

    started implementing level selection section

---
+++ Scenes/LevelBox/LevelBox.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+
+public partial class LevelBox : Button
+{
+
+    public int levelNumber {get; private set;} = -1;
+
+    private Label label;
+    private Sprite2D buttonSprite;
+
+    public override void _Ready()
+    {
+        this.label = (Label) FindChild("Label");
+        this.label.Text = "";
+
+        this.buttonSprite = (Sprite2D) FindChild("Sprite2D");
+        this.buttonSprite.Frame = 0;
+        
+        this.Pressed += this.onPressed;
+    }
+
+    public void SetLevelNumber(int number)
+    {
+        this.levelNumber = number;
+        this.label.Text = this.levelNumber.ToString();
+    }
+
+    public void onPressed()
+    {
+        //
+    }
+}
